f3046cfbcb77104af2181f9fda072aeaf5feffdf
isyamil <zaynhc348@gmail.com>
2025-01-16 17:20:30 -0500

    Primer commit2

--- calculadora_basica.py
+++ calculadora_basica.py
@@ -3,13 +3,15 @@ operacion = int(operacion)
 number1 = int(input('Dime el primero número: '))
 number2 = int(input('Dime el segundo número: '))
 
-if operacion == 1:
-    print(f'Tu resultado es: {number1 + number2}')
-elif operacion == 2:
-    print(f'Tu resultado es: {number1 - number2}')
-elif operacion == 3:
-    print(f'Tu resultado es: {number1 * number2}')
-elif operacion == 4:
-    print(f'Tu resultado es: {number1 / number2}')
-else:
-    print('Hubo un error')
+def calculadora():
+    if operacion == 1:
+        print(f'Tu resultado es: {number1 + number2}')
+    elif operacion == 2:
+        print(f'Tu resultado es: {number1 - number2}')
+    elif operacion == 3:
+        print(f'Tu resultado es: {number1 * number2}')
+    elif operacion == 4:
+        print(f'Tu resultado es: {number1 / number2}')
+    else:
+        print('Hubo un error')
+
